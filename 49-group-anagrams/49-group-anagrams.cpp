461f20a0c52b2e10135b0543a33b5c63b73c10e2
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-03-21 08:46:58 +0530

    Time: 51 ms (56.50%), Space: 19.6 MB (72.49%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string,vector<string>> map;
+        
+        for(auto& s : strs)
+        {
+            string t = s;
+            sort(t.begin(),t.end());
+            map[t].push_back(s);
+        }
+        
+        for(auto& it : map)
+            res.push_back(it.second);
+        
+        return res;
+    }
+};
