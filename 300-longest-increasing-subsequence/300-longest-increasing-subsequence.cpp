eb277268b75148ae28c146726206160c2da4141f
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-03-24 13:11:29 +0530

    Time: 8 ms (94.05%), Space: 10.5 MB (64.43%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -17,16 +17,18 @@ public:
         }
         return res;
     }*/
+    //This approach uses DP so it takes TC of O(N^2) and SC of O(N)
     int lengthOfLIS(vector<int>& nums){
         vector<int> res;
         for(int &n : nums){
             if(res.empty() || res[res.size()-1]<n)
                 res.push_back(n);
             else{
-                auto it = lower_bound(res.begin(),res.end(),n);
-                *it = n;
+                int it = lower_bound(res.begin(),res.end(),n)-res.begin();
+                res[it] = n;
             }
         }
         return res.size();
     }
+    //This approach use Binary Search inside the resultant subsequence array to find the smallest number that is greater than n and replaces n with that number. This works because we assume that it is the best to keep the smallest number in the subsequence for the possiblity of the sequence becoming the largest.
 };
