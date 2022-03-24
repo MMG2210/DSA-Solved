8f5f502dbf26dc614ec5df06dfa9b7b5a4cef5df
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-03-24 13:08:11 +0530

    Time: 13 ms (81.80%), Space: 10.5 MB (38.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    /*int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int> dis(n,1);
         int res=1;
@@ -16,5 +16,17 @@ public:
             res = max(res,dis[i]);
         }
         return res;
+    }*/
+    int lengthOfLIS(vector<int>& nums){
+        vector<int> res;
+        for(int &n : nums){
+            if(res.empty() || res[res.size()-1]<n)
+                res.push_back(n);
+            else{
+                auto it = lower_bound(res.begin(),res.end(),n);
+                *it = n;
+            }
+        }
+        return res.size();
     }
 };
