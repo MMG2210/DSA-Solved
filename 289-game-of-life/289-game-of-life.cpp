788b44c09cf3a274885ed6808b876a0bf7309c51
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-03-29 08:24:14 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (59.05%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countNeighbors(vector<vector<int>>& board, int i, int j, int m, int n) {
+        int count = 0;
+        
+        for (int I = max(i-1, 0); I < min(i + 2, m); ++I)
+            for (int J = max(j - 1, 0); J < min(j + 2, n); ++J) {
+                if(i == I && j == J) continue;//Don't check the same element only neighbors.
+                
+                if(board[I][J] == 1 || board[I][J] == 2)//If it is alive or newly dead ie., alive in
+                    count++;// the original state but dead in new/next state.
+            }
+                 
+        return count;
+    }
+    void gameOfLife(vector<vector<int>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int countOfNeighbors = countNeighbors(board,i,j,n,m);
+                if(board[i][j]==1){
+                    if(countOfNeighbors<2 || countOfNeighbors>3){
+                        board[i][j]=2;//2 means newly dead or dead in next state but alive in original
+                    }
+                }
+                else if(board[i][j]==0){
+                    if(countOfNeighbors==3)
+                        board[i][j]=3;//3 means newly alive or alive in next state but dead in original
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                board[i][j]%=2;//since newly deads are 2, newly alive are 3,
+                //2%2=0(dead) and similarly 3%2=1(alive).
+            }
+        }
+    }//TC is O(N^2) while SC is O(1)
+};
