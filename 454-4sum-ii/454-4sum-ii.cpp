a1e9f071fdd786cbff24013f9eb3cf9914a6897f
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-03-30 08:26:55 +0530

    Time: 446 ms (31.55%), Space: 24.2 MB (99.46%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        int count=0;
+        int res=0;
         int n = nums1.size();
-        unordered_map<int,int> mp;
-        for (auto &it1: nums1)
-            for (auto &it2:nums2)
-                mp[it1+it2]++; 
-        
-        for(auto &it3: nums3)
-            for(auto &it4:nums4)
-               if(mp.count(0-it3-it4)) count+=mp[0-it3-it4];
-        
-        return count;
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int tempSum = nums1[i]+nums2[j];
+                if(m.find(tempSum)==m.end()){
+                    m[tempSum] = 1;
+                }
+                else m[tempSum]++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int tempSum = nums3[i]+nums4[j];
+                tempSum*=-1;
+                if(m.find(tempSum)!=m.end())res+=m[tempSum];
+            }
+        }
+        return res;
     }
 };
