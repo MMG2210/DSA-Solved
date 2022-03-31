8fb3bf7cb5ae51c883688c8c9ec685dd721de71f
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-03-31 20:55:29 +0530

    Time: 16 ms (67.90%), Space: 17.3 MB (94.36%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -18,7 +18,7 @@ public:
 
 class Solution {
 public:
-    Node* connect(Node* root) {
+    /*Node* connect(Node* root) {
         if(root==NULL)return root;
         queue<Node*> q;
         q.push(root);
@@ -36,6 +36,27 @@ public:
                 q.push(NULL);
             }
         }
+        return root;
+        TC - O(N),SC - O(N)
+    }*/
+    Node* connect(Node* root){
+        if(!root)return NULL;
+        Node* p = root->next;
+        while(p){
+            if(p->left){
+                p=p->left;
+                break;
+            }
+            if(p->right){
+                p=p->right;
+                break;
+            }
+            p=p->next;
+        }
+        if(root->right)root->right->next=p;
+        if(root->left)root->left->next=root->right?root->right:p;
+        connect(root->right);
+        connect(root->left);
         return root;
     }
 };
