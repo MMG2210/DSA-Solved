22b7a2227594552d8275554edd672a1aa0784e4f
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-03-31 09:01:20 +0530

    Time: 15 ms (50.08%), Space: 11.8 MB (98.40%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
+    bool notOperation(string t){
+        if(t == "+" || t == "-" || t == "*" || t == "/")return false;
+        else return true;
+    }
+    /*int evalRPN(vector<string>& tokens) {
         stack<int> s;
         for(auto& t : tokens){
 		    if(t == "+" || t == "-" || t == "*" || t == "/") {
@@ -15,5 +19,22 @@ public:
 		    else s.push(stoi(t));    // stoi - converts from string to int 
         }    
 	    return s.top();
+    TC = O(N) SC = O(N) for the stack
+    }*/
+    int evalRPN(vector<string>& tokens){
+        int top=0;
+        for(auto &a : tokens){
+            if(notOperation(a))tokens[top++]=a;
+            else{
+                int op1 = stoi(tokens[--top]);
+                int op2 = stoi(tokens[--top]);
+                if(a == "+") op1 = op2 + op1;
+			    if(a == "-") op1 = op2 - op1;
+			    if(a == "/") op1 = op2 / op1;
+			    if(a == "*") op1 = op2 * op1;
+                tokens[top++]=to_string(op1);
+            }
+        }
+        return stoi(tokens[0]);
     }
 };
