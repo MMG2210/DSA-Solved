d297eac8ef205819884942b11329b7c93948e8f3
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-04-06 20:15:18 +0530

    Time: 7 ms (93.50%), Space: 10.6 MB (24.99%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int count=0;
         unordered_set<int> vis;
         queue<int> q;
         q.push(0);
         vis.insert(0);
-        count++;
         while(!q.empty()){
             int temp = q.front();q.pop();
             for(int& i:rooms[temp]){
                 if(vis.find(i)==vis.end()){
-                    count++;
                     vis.insert(i);
                     q.push(i);
                 }
             }
         }
-        return count==rooms.size();
+        return vis.size()==rooms.size();
     }
 };
