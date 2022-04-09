d132202de3a442cf3bdea5378fc8d3e6dcb30f03
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-04-09 12:09:12 +0530

    Time: 11 ms (50.44%), Space: 13 MB (80.18%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
-    bool isValid(vector<vector<int>>& grid, int i, int j){
-        return i>=0 && i<grid.size() && j>=0 && j<grid[0].size() && grid[i][j]==1;
-    }
-    int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size(), n=grid[0].size();
-        queue<pair<int,int>> rotten;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2)rotten.push({i,j});
+    int orangesRotting(vector<vector<int>>& grid) 
+    {
+        
+        vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
+        
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        queue<pair<int,int>> q;
+        int fresh=0; //To keep track of all fresh oranges left
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==2)
+                    q.push({i,j});
+                if(grid[i][j]==1)
+                    fresh++;
             }
-        }
-        bool anyFresh=false;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1)anyFresh=true;
+        int ans=-1; //initialised to -1 since after each step we increment the time by 1 and initially all rotten oranges started at 0.
+        while(!q.empty())
+        {
+            int sz=q.size();
+            while(sz--)
+            {
+                pair<int,int> p=q.front();
+                q.pop();
+                for(int i=0;i<4;i++)
+                {
+                    int r=p.first+dir[i];
+                    int c=p.second+dir[i+1];
+                    if(r>=0 && r<m && c>=0 && c<n &&grid[r][c]==1)
+                    {
+                        grid[r][c]=2;
+                        q.push({r,c});
+                        fresh--; // decrement by 1 foreach fresh orange that now is rotten
+                    }
+                    
+                }
             }
+            ans++; //incremented after each minute passes
         }
-        int mins=-1;
-        if(!anyFresh)return 0;
-        while(!rotten.empty()){
-            int s = rotten.size();
-            for(int it=0;it<s;it++){
-                auto temp = rotten.front();rotten.pop();
-                int i=temp.first,j=temp.second;
-                if(isValid(grid,i+1,j)){rotten.push({i+1,j});grid[i+1][j]=2;}
-                if(isValid(grid,i-1,j)){rotten.push({i-1,j});grid[i-1][j]=2;}
-                if(isValid(grid,i,j+1)){rotten.push({i,j+1});grid[i][j+1]=2;}
-                if(isValid(grid,i,j-1)){rotten.push({i,j-1});grid[i][j-1]=2;}
-            }
-            mins++;
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1)return -1;
-            }
-        }
-        return mins;
+        if(fresh>0) return -1; //if fresh>0 that means there are fresh oranges left
+        if(ans==-1) return 0; //we initialised with -1, so if there were no oranges it'd take 0 mins.
+        return ans;
     }
 };
