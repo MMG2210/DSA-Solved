612cfd218e3bfeb4e035fc8d5a8b3b218509765f
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-04-09 15:25:26 +0530

    Time: 104 ms (38.83%), Space: 19.8 MB (63.33%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        int maxDis=0,m=grid.size(),n=grid[0].size();
-        queue<pair<int,int>> q;
+        int locMax = 0;
+		queue<pair<int, int>> q;
+		for (int i = 0; i < grid.size(); ++i) {
+			for (int j = 0; j < grid[i].size(); ++j) {
+				if (grid[i][j] == 1) q.push({ i, j });
+			}
+		}
 
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-                if(grid[i][j]==1)q.push({i,j});
-        
-        while(!q.empty()){
-            vector<pair<int,int>> newDis = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
-            auto temp = q.front();q.pop();
-            for(int i=0;i<4;i++){
-                int nx = temp.first + newDis[i].first;
-                int ny =temp.second + newDis[i].second;
-                if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==0){
-                    q.push({nx,ny});
-                    grid[nx][ny]=grid[temp.first][temp.second]+1;
-                    maxDis = max(maxDis,grid[nx][ny]);
-                }
-            }
-        }
-        return maxDis>0? maxDis-1:-1;
+		while (!q.empty()) {
+			pair<int, int> cur = q.front();
+			q.pop();
+			pair<int, int> dirs[4] = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
+			for (auto dir : dirs) {
+				int nx = dir.first + cur.first;
+				int ny = dir.second + cur.second;
+				if (nx >= 0 && nx < grid.size() && ny >= 0 && ny < grid[0].size() && grid[nx][ny] == 0) {
+					q.push({ nx, ny });
+					grid[nx][ny] = grid[cur.first][cur.second] + 1;
+					locMax = max(grid[nx][ny], locMax);
+				}
+			}
+		}
+		return (locMax > 0 ? locMax-1 : -1);
     }
 };
