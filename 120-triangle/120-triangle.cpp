291f6e1e69caf817ed03d6a2de3713f8fdea6a06
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-04-12 07:39:13 +0530

    Time: 4 ms (92.15%), Space: 8.6 MB (68.29%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -5,23 +5,10 @@ public:
         if(dp[h][j]!=-1)return dp[h][j];
         return dp[h][j]=triangle[h][j]+min(util(triangle,h+1,j,dp),util(triangle,h+1,j+1,dp));
     }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int h=triangle.size(),j;
-        j=h;
-        vector<vector<int>> v(h,vector<int> (j,0));
-         v[0][0]=triangle[0][0];
-         for(int i=1;i<h;i++)
-         {
-             for(int j=0;j<triangle[i].size();j++)
-             {
-                 if(j==0) v[i][j]=v[i-1][j]+triangle[i][j];
-                 else if(j==triangle[i].size()-1) v[i][j]=v[i-1][j-1]+triangle[i][j];
-                 else
-                 v[i][j]=min(v[i-1][j-1],v[i-1][j])+triangle[i][j];
-             }
-              
-         }
-         
-        return *min_element(v[h-1].begin(),v[h-1].end());
+    int minimumTotal(vector<vector<int>>& T) {
+        for (int i = T.size() - 2; ~i; i--) 
+            for (int j = T[i].size() - 1; ~j; j--) 
+                T[i][j] += min(T[i+1][j], T[i+1][j+1]);
+        return T[0][0];
     }
 };
