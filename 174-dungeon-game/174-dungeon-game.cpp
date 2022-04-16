8fbf69f5d66b10e3bc64837da2b3dbcb15422232
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-04-16 09:09:12 +0530

    Time: 8 ms (66.81%), Space: 9.1 MB (17.72%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int m,n;
-    int recurse(vector<vector<int>>& dungeon,int i, int j, vector<vector<int>>& dp){
+    /*int recurse(vector<vector<int>>& dungeon,int i, int j, vector<vector<int>>& dp){
         if(i==m || j==n)return 1e9;
         
         if(i==m-1 && j==n-1){
@@ -20,5 +20,25 @@ public:
         m=dungeon.size();
         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
         return recurse(dungeon,0,0,dp);
+    }*/
+    
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        n=dungeon[0].size();
+        m=dungeon.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,INT_MAX));
+        
+        dp[m][n-1]=1;
+        dp[m-1][n]=1;
+        
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int right=dp[i][j+1];
+                int down=dp[i+1][j];
+                int res=min(right,down)-dungeon[i][j];
+                dp[i][j] = res<=0?1:res;
+            }
+        }
+        
+        return dp[0][0];
     }
 };
