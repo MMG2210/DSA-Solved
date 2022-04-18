295a82a33ba851722f764d3af3c0d2ec002a9c60
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-04-18 08:20:53 +0530

    Time: 12 ms (97.41%), Space: 24.1 MB (87.81%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -15,7 +15,7 @@ public:
     if (root) {
         int x = kthSmallest(root->left, k);
         return !k ? x : !--k ? root->val : kthSmallest(root->right, k);
-    }
+        }
     return -1;
     }
 };
