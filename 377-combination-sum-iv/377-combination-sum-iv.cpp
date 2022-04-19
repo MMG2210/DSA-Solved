698bd3166a67e87fefb6f5f2f2e65af22b7f7335
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-04-19 22:11:46 +0530

    Time: 4 ms (39.67%), Space: 6.7 MB (33.59%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int ind, int trgt, vector<vector<int>>& dp){
+    /*int solve(vector<int>& nums, int ind, int trgt, vector<vector<int>>& dp){
         if(trgt==0)return 1;
         if(ind==nums.size())return 0;
         if(dp[ind][trgt]!=-1)return dp[ind][trgt];
@@ -12,17 +12,16 @@ public:
         int n=nums.size();
         vector<vector<int>> dp(nums.size()+1,vector<int>(target+1,-1));
         return solve(nums,0,target,dp);
-        /*for(int i=0;i<nums.size();i++){
-            dp[i][0]=1;
-        }
-        for(int i=0;i<=target;i++)
-            dp[n][i]=0;
-        for(int i=n-1;i>=0;i--){
-            for(int j=1;j<=target;j++){
-                if(nums[i]>j){
-                    dp[i][j]=
-                }
+    }*/
+    int combinationSum4(vector<int>& nums, int target){
+        int n=nums.size();
+        vector<unsigned int> dp(target+1,0);
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(int& n:nums){
+                if(n<=i)dp[i]+=dp[i-n];
             }
-        }*/
+        }
+        return dp[target];
     }
 };
