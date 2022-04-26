5ff0df7c6d7348e7508a6cab749a092b2be4b85a
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-04-26 12:52:16 +0530

    Time: 952 ms (33.79%), Space: 130.2 MB (25.60%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n=points.size();
+        vector<pair<int,int>> adj[n];
+        int res=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j)continue;
+                int dist = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                adj[i].push_back({j,dist});
+            }
+        }
+        
+        vector<bool> vis(n,false);
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> edges;
+        edges.push({0,0});
+        
+        while(!edges.empty()){
+            pair<int,int> t=edges.top(); edges.pop();
+            int node=t.second;
+            int cost=t.first;
+            if(vis[node])continue;
+            vis[node]=true;
+            res+=cost;
+            for(auto& it:adj[node]){
+                if(vis[it.first])continue;
+                else{
+                    edges.push({it.second,it.first});
+                }
+            }
+        }
+        
+        return res;
+    }
+};
