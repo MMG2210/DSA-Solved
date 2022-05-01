93562eef83e1a30aba2515db48ad4c8ffefc8a99
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-05-01 14:43:01 +0530

    Time: 4 ms (25.00%), Space: 7 MB (50.00%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
@@ -3,22 +3,23 @@ public:
     string removeDigit(string number, char digit) {
         string res="";
         int n=number.size();
-        vector<string> combos;
+        //vector<string> combos;
         for(int i=0;i<n;i++){
             if(number[i]==digit){
                 string temp = number.substr(0,i)+number.substr(i+1,n);
-                combos.push_back(temp);
+                //combos.push_back(temp);
+                res=max(temp,res);
             }
         }
         
-        res=combos[0];
+        //res=combos[0];
         
 //         int csize=combos.size();
         
 //         sort(combos.begin(), combos.end());
-        for(string& c:combos){
-            res=max(res,c);
-        }
+        // for(string& c:combos){
+        //     res=max(res,c);
+        // }
         
         return res;
     }
