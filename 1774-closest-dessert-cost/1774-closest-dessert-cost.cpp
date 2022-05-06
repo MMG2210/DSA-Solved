8bd8f3075c1a42a16c8c98133a795b1fb28acaea
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-05-06 08:27:03 +0530

    Time: 47 ms (47.93%), Space: 9.8 MB (40.50%) - LeetHub

---
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int t;
+    
+    int closest(int a, int b){
+        if(a==0)return b;
+        if(b==0)return a;
+        if(abs(t-a)==abs(t-b))return a<b?a:b;
+        return abs(t-a)<abs(t-b)?a:b;
+    }
+    
+    int recur(vector<int>& tC, int i, int cost){
+        if(i==tC.size())return cost;
+        
+        int skip=recur(tC,i+1,cost);
+        int take1=recur(tC,i+1,cost+(1*tC[i]));
+        int take2=recur(tC,i+1,cost+(2*tC[i]));
+        return closest(skip, closest(take1,take2));
+    }
+    
+    int closestCost(vector<int>& baseCosts, vector<int>& tC, int target) {
+        t=target;
+        int res=0;
+        for(int& bc:baseCosts){
+            res=closest(res, recur(tC,0,bc));
+        }
+        
+        return res;
+    }
+};
