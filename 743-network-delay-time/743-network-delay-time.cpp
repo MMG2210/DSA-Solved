bedb5ff84b0643aaedfa17ebae53ace488daf93a
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-05-14 08:35:59 +0530

    Time: 124 ms (86.73%), Space: 40 MB (71.55%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> dist(n+1,1e9);
-        vector<vector<pair<int,int>>> adj(n+1);
-        for(auto& t:times){
-            adj[t[0]].push_back({t[1],t[2]});
-        }
-        
+    int dijkstra(vector<int>& dist,vector<vector<pair<int,int>>>& adj, int& n, int& k){
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         dist[k]=0;
         pq.push({0,k});
@@ -28,5 +22,15 @@ public:
         }
         
         return res==1e9?-1:res;
+    }
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<int> dist(n+1,1e9);
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto& t:times){
+            adj[t[0]].push_back({t[1],t[2]});
+        }
+        
+        return dijkstra(dist,adj,n,k);
     }
 };
