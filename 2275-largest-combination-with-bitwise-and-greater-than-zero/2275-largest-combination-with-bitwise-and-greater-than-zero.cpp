ac26627d58e1eb1f9fc67eac9dad885754f83186
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-05-15 09:47:51 +0530

    Time: 271 ms (33.33%), Space: 57.6 MB (33.33%) - LeetHub

---
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int res=0;
+        for(int i=31;i>=0;i--){
+            int cnt=0;
+            for(int&c:candidates){
+                if((c>>i)&1)cnt++;
+            }
+            res=max(res,cnt);
+        }
+        return res;
+    }
+};
