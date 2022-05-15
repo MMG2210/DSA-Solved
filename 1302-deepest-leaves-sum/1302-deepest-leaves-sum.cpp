347acf5c6bcd7c003185eadff569aa3125ab6987
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-05-15 07:14:49 +0530

    Time: 152 ms (28.89%), Space: 61.8 MB (43.30%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -13,36 +13,51 @@ class Solution {
 public:
     //int h=INT_MIN;
     
-    int findMaxHeight(TreeNode* root){
-        if(!root)return 0;
+//     int findMaxHeight(TreeNode* root){
+//         if(!root)return 0;
         
-        int lh=findMaxHeight(root->left);
-        int rh=findMaxHeight(root->right);
+//         int lh=findMaxHeight(root->left);
+//         int rh=findMaxHeight(root->right);
         
-        return 1+max(lh,rh);
-    }
+//         return 1+max(lh,rh);
+//     }
     
-    int deepestLeavesSum(TreeNode* root) {
-        int lvls = findMaxHeight(root),res=0;
+//     int deepestLeavesSum(TreeNode* root) {
+//         int lvls = findMaxHeight(root),res=0;
         
-        queue<TreeNode*> q;
-        int curLevel=1;
-        q.push(root);
-        q.push(NULL);
-        while(!q.empty()){
-            TreeNode* f=q.front();q.pop();
-            if(f==NULL){
-                if(q.empty())break;
-                curLevel++;
-                q.push(NULL);
-            }
+//         queue<TreeNode*> q;
+//         int curLevel=1;
+//         q.push(root);
+//         q.push(NULL);
+//         while(!q.empty()){
+//             TreeNode* f=q.front();q.pop();
+//             if(f==NULL){
+//                 if(q.empty())break;
+//                 curLevel++;
+//                 q.push(NULL);
+//             }
             
-            else{
-                if(f->left)q.push(f->left);
-                if(f->right)q.push(f->right);
-                if(curLevel==lvls){
-                    res+=f->val;
-                }
+//             else{
+//                 if(f->left)q.push(f->left);
+//                 if(f->right)q.push(f->right);
+//                 if(curLevel==lvls){
+//                     res+=f->val;
+//                 }
+//             }
+//         }
+        
+//         return res;
+//     }
+    int deepestLeavesSum(TreeNode* root) {
+        queue<TreeNode*> q;
+        int res=0,i;
+        q.push(root);
+        while(!q.empty()){
+            for(i=q.size()-1,res=0;i>=0;i--){
+                TreeNode* node=q.front(); q.pop();
+                res+=node->val;
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
             }
         }
         
