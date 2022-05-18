d1e6da27379ce959f42d59864f016135983b7c9e
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-05-18 17:18:31 +0530

    Time: 777 ms (64.55%), Space: 164.6 MB (91.65%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,62 @@
+#define pb push_back
+class Solution {
+public:
+    
+//     int dfs(int start,vector<vector<int>>& adj,vector<int>& ex,vector<bool>& vis){
+//         vis[start]=true;
+//         int cnt=1;
+//         for(auto& a:adj[start]){
+//             int ex1=ex[0],ex2=ex[1];
+//             if((start==ex1 && a==ex2) || (start==ex2 && a==ex1))continue;
+//             if(!vis[a]){
+//                 cnt+=dfs(a,adj,ex,vis);
+//             }
+//         }
+//         return cnt;
+//     }
+    
+//     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+//        vector<vector<int>> adj(n);
+//         for(auto& c:connections){
+//             adj[c[0]].push_back(c[1]);
+//             adj[c[1]].push_back(c[0]);
+//         }
+//         vector<vector<int>> res;
+//         for(int i=0;i<connections.size();i++){
+//             vector<int> exclude(2);
+//             vector<bool> vis(n,false);
+//             exclude[0]=connections[i][0];
+//             exclude[1]=connections[i][1];
+//             int count = dfs(0,adj,exclude,vis);
+//             if(count<n)res.push_back(exclude);
+//         }
+//         return res;
+//     } Straight-up DFS TC - O(E*(V+E));
+    
+    void tarjans(int curr, int parent, int time, vector<vector<int>>& adj, vector<int>& low, vector<vector<int>>& res){
+        low[curr]=time++;
+        
+        for(auto& x:adj[curr]){
+            if(x==parent)continue;
+            if(low[x]==0){
+                tarjans(x,curr,time,adj,low,res);
+            }
+            low[curr]=min(low[curr],low[x]);
+            if(low[x]==time){
+                res.push_back({curr,x});
+            }
+        }
+    }
+    
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+       vector<vector<int>> adj(n);
+        for(auto& c:connections){
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(c[0]);
+        }
+        vector<vector<int>> res;
+        vector<int> low(n);
+        tarjans(0,-1,1,adj,low,res);
+        return res;
+    }
+};
