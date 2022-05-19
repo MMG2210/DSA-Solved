ce70165776b78101973d458d27ae8ea7a5eccc01
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-05-19 08:34:04 +0530

    Time: 54 ms (71.93%), Space: 14.8 MB (95.23%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int res=INT_MIN;
+    int mod = 1e9+7;
+    
+    int dp[201][201];
+    
+    bool isInvalid(vector<vector<int>>& matrix, int i, int j){
+        return i<0 || j<0 || i>matrix.size()-1 || j>matrix[i].size()-1;
+    }
+    
+    int dfs(vector<vector<int>>& matrix, int i, int j, int prev){
+        if(isInvalid(matrix,i,j))return 0;
+        if(prev>=matrix[i][j])return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        int len=0;
+        len=max(len,dfs(matrix, i-1, j, matrix[i][j]));
+        len=max(len,dfs(matrix, i+1, j, matrix[i][j]));
+        len=max(len,dfs(matrix, i, j-1, matrix[i][j]));
+        len=max(len,dfs(matrix, i, j+1, matrix[i][j]));
+        return dp[i][j] = (1+len)%mod;
+    }
+    
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m=matrix.size(), n=matrix[0].size();
+        memset(dp,-1,sizeof(dp));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                res=max(res,dfs(matrix,i,j,-1));
+            }
+        }
+        
+        return res;
+    }
+};
