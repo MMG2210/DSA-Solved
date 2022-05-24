d612f1ae095a15597e58d8b934bbc5ed4bd85133
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-05-24 06:42:06 +0530

    Time: 8 ms (37.09%), Space: 6.7 MB (96.04%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> st;
-        st.push(-1);
-        int res=0;
+//         stack<int> st;
+//         st.push(-1);
+//         int res=0;
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]=='('){
+//                 st.push(i);
+//             }
+//             else{
+//                 st.pop();
+//                 if(st.empty()){
+//                     st.push(i);
+//                 }
+//                 else res=max(res,i-st.top());
+//             }
+//         }
+        
+//         return res;
+        int res=0,left=0,right=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                st.push(i);
+            if(s[i]=='(')left++;
+            else right++;
+            if(left==right)res=max(res,right*2);
+            else if(right>left){
+                left=right=0;
             }
-            else{
-                st.pop();
-                if(st.empty()){
-                    st.push(i);
-                }
-                else res=max(res,i-st.top());
+        }
+        left=right=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='(')left++;
+            else right++;
+            if(left==right)res=max(res,left*2);
+            else if(right<left){
+                left=right=0;
             }
         }
         
