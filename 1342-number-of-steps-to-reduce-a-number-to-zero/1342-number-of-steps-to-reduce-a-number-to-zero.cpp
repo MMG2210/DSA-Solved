9b7407fabe6fc129a0179dfbe30a00f2d5c9f811
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-05-27 06:01:52 +0530

    Time: 5 ms (14.10%), Space: 5.9 MB (26.66%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int res=0;
+        while(num){
+            res++;
+            if(num&1)num-=1;
+            else num/=2;
+        }
+        
+        return res;
+    }
+};
