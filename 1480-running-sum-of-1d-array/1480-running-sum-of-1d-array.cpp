ea947af4b52b9be78cdc830e570a609dd0e80c44
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-06-01 07:59:33 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (38.06%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> preSum(n,0);
+        preSum[0]=nums[0];
+        for(int i=1;i<n;i++){
+            preSum[i]=nums[i]+preSum[i-1];
+        }
+        
+        return preSum;
+    }
+};
