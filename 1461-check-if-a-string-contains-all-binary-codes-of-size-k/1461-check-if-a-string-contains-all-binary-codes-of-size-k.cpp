65a507312749bdb67a6b573ec69d8fbaa2747dc7
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-06-01 20:53:00 +0530

    Time: 94 ms (88.51%), Space: 19 MB (90.06%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    int binExpo(int a, int b){
-        int res=1,mod=1e9+7;
-        long long A=a;
-        while(b){
-            if(b&1)res*=A;
-            res%=mod;
-            A*=A;
-            A%=mod;
-            b>>=1;
-        }
-        return res;
-    }
-    
     bool hasAllCodes(string s, int k) {
-        if(k>s.size())return 0;
-        unordered_set<string> st;
-        for(int i=0;i<s.size()-k+1;i++){
-            st.insert(s.substr(i,k));
+        int n = s.size(), size=1<<k, count=0, hash=0, allOnes=~size;
+        vector<bool> set(size,false);
+        
+        for(int i=0;i<n;i++){
+            hash=((hash<<1)&allOnes)|(s[i]-'0');
+            if(i>=k-1 && !set[hash]){
+                count++;
+                set[hash]=true;
+            }
         }
         
-        //return st.size()==pow(2,k);
-        return st.size()==(1<<k);
+        return size==count;
     }
 };
