efd616df910a21ecc71b0988da8f20fe13f97832
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-06-09 08:37:18 +0530

    Time: 231 ms (47.83%), Space: 83.1 MB (40.98%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
+    //int firstMissingPositive(vector<int>& nums) {
+    //     unordered_set<int> s;
+    //     for(int& n:nums){
+    //         if(n>0)s.insert(n);
+    //     }
+    //     for(int i=1;i<1e9;i++){
+    //         if(s.find(i)==s.end())return i;
+    //     }
+    //     return -1;
+    // }
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> s;
-        for(int& n:nums){
-            if(n>0)s.insert(n);
-        }
-        for(int i=1;i<1e9;i++){
-            if(s.find(i)==s.end())return i;
-        }
-        return -1;
+        int n=nums.size();
+        for(int i=0;i<n;i++)if(nums[i]<=0)nums[i]=n+1;
+        for(int i=0;i<n;i++)if(abs(nums[i])<=n && nums[abs(nums[i])-1]>0)nums[abs(nums[i])-1]*=-1;
+        for(int i=0;i<n;i++)if(nums[i]>0)return i+1;
+        return n+1;
     }
 };
