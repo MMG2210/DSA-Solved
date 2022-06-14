efa4c3d5bb2bb4a368a0fa5ccc72054262b92480
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-06-14 09:40:16 +0530

    Time: 37 ms (36.17%), Space: 12.1 MB (49.22%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    int N,M;
-    int minDistance(string word1, string word2) {
-        N=word1.size();
-        M=word2.size();
-        vector<vector<int>> dp(N+1,vector<int>(M+1,-1));
-        int lcs=solve(word1,word2,0,0,dp);
-        return N+M-2*lcs;
-    }
+       int N,M;
+//     int minDistance(string word1, string word2) {
+//         N=word1.size();
+//         M=word2.size();
+//         vector<vector<int>> dp(N+1,vector<int>(M+1,-1));
+//         int lcs=solve(word1,word2,0,0,dp);
+//         return N+M-2*lcs;
+//     }
     
-    int solve(string &w1, string& w2, int i, int j, vector<vector<int>>& dp){
-        if(i==N || j==M)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
+//     int solve(string &w1, string& w2, int i, int j, vector<vector<int>>& dp){
+//         if(i==N || j==M)return 0;
+//         if(dp[i][j]!=-1)return dp[i][j];
         
-        if(w1[i]==w2[j])return dp[i][j]=1+solve(w1,w2,i+1,j+1,dp);
-        else return dp[i][j]=max(solve(w1,w2,i+1,j,dp), solve(w1,w2,i,j+1,dp));
-    }
+//         if(w1[i]==w2[j])return dp[i][j]=1+solve(w1,w2,i+1,j+1,dp);
+//         else return dp[i][j]=max(solve(w1,w2,i+1,j,dp), solve(w1,w2,i,j+1,dp));
+//     }
+        int minDistance(string w1, string w2) {
+            N=w1.size();
+            M=w2.size();          
+            vector<vector<int>> dp(N+1,vector<int>(M+1,0));
+
+            for(int i=1;i<=N;i++){
+                for(int j=1;j<=M;j++){
+                    if(w1[i-1]==w2[j-1])dp[i][j]=1+dp[i-1][j-1];
+                    else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+            return N+M - 2*dp[N][M];
+        }   
 };
