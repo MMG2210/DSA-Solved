59bfb0a6a497480040ee962dd70968a85b1d62a9
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-06-14 21:41:13 +0530

    Time: 29 ms (7.82%), Space: 10.6 MB (43.28%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -4,6 +4,19 @@
 class Solution {
 public:
     int M,N;
+    
+    int findFirst0(vector<vector<int>>& mat, int i){
+        int l=0,r=N-1,res=N;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(!mat[i][m]){
+                res=m;
+                r=m-1;
+            }
+            else l=m+1;
+        }
+        return res;
+    }
 
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         vector<int> res;
@@ -11,9 +24,9 @@ public:
         N=mat[0].size();
         priority_queue<pii, vector<pii>, greater<pii>> pq;
         for(int i=0;i<M;i++){
-            int j=0;
-            while(j<N && mat[i][j]==1)j++;
-            pq.push({j,i});
+            // int j=0;
+            // while(j<N && mat[i][j]==1)j++;
+            pq.push({findFirst0(mat,i),i});
         }
         
         while(k--){
