19a0892cee0dcf8b021da9fdf4a146fd0f31c871
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-06-25 10:25:57 +0530

    Time: 22 ms (98.60%), Space: 27.1 MB (12.14%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int pos=-1, shift=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]>nums[i]){
+                shift++;
+                if(i-2<0 || nums[i-2]<=nums[i])nums[i-1]=nums[i];
+                else nums[i]=nums[i-1];
+            }
+        }
+        
+        return shift<=1;
+    }
+};
