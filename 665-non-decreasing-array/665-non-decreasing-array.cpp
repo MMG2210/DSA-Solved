e44647e4e045f082fbc78a2efa1e4e487aeb5de9
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-06-25 10:32:07 +0530

    Time: 29 ms (87.21%), Space: 27.1 MB (12.14%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
         int shift=0;
-        for(int i=1;i<nums.size();i++){
+        for(int i=1;i<nums.size() && shift<=1;i++){
             if(nums[i-1]>nums[i]){
                 shift++;
                 if(i-2<0 || nums[i-2]<=nums[i])nums[i-1]=nums[i];
