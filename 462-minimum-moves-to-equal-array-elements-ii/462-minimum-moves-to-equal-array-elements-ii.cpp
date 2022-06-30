63bc10fd9e6fa6edd24acb8b4662ab71e8e8afab
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-06-30 13:15:45 +0530

    Time: 12 ms (81.15%), Space: 10.9 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,8 +4,12 @@ public:
         int res=0, n=nums.size();
         sort(nums.begin(),nums.end());
         int mid = nums[n/2];
-        for(int i=0;i<n;i++){
-            res+=abs(nums[i]-mid);
+        int l=0,r=n-1;
+        // for(int i=0;i<n;i++){
+        //     res+=abs(nums[i]-mid);
+        // }
+        while(l<r){
+            res+=nums[r--]-nums[l++];
         }
         return res;
     }
