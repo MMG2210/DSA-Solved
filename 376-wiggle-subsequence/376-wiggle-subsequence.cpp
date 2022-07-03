354c56af255b6a47a4ddd51d2573cd2d80d9da1e
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-07-03 18:46:34 +0530

    Time: 32 ms (8.74%), Space: 7.6 MB (7.34%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int wiggleMaxLength(vector<int>& nums) {
-	int n = size(nums), len = 1, prevDiff = 0, currDiff;
-	for(int i = 1; i < n; i++){
-		currDiff = nums[i] - nums[i - 1];
-		if((currDiff > 0 && prevDiff <= 0) || (currDiff < 0 && prevDiff >= 0))
-			len++, prevDiff = currDiff;
-	}
-	return len;
-}
+    int wiggleMaxLength(vector<int>& a) {
+        int n=a.size();
+        vector<vector<int>> dp(n,vector<int>(2,1));
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(a[i]>a[j] && dp[i][0] < 1+dp[j][1])dp[i][0]=dp[j][1]+1;
+                else if(a[i]<a[j] && dp[i][1] < 1 + dp[j][0])dp[i][1] = dp[j][0]+1;
+            }
+        }
+        return max(dp[n-1][0],dp[n-1][1]);
+    }
 };
