6e6dad6b5066e4c9354d58d32ed850c2b71d1ffa
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-07-10 09:53:09 +0530

    Time: 331 ms (46.11%), Space: 80.8 MB (70.76%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        deque<int> d{0};
-        for (int i = 1; i < nums.size(); ++i) {
-            if (d.front() + k < i)
-                d.pop_front();        
-                nums[i] += nums[d.front()];
-                while (!d.empty() && nums[d.back()] <= nums[i])
-                    d.pop_back();
-                d.push_back(i);
-            }
-            return nums.back();
+        int n=nums.size();
+        vector<int> dp(n,INT_MIN);
+        deque<int> dq{0};
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            if(dq.front()<i-k)dq.pop_front();
+            dp[i]=nums[i]+dp[dq.front()];
+            while(!dq.empty() && dp[i]>=dp[dq.back()])dq.pop_back();
+            dq.push_back(i);
         }
+        return dp.back();
+    }
 };
