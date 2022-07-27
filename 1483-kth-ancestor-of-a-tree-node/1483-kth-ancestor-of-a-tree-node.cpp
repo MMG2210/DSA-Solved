527fb8181eb83895117c14115d20cda8f75ffae4
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-07-27 15:08:55 +0530

    Time: 382 ms (86.20%), Space: 116.8 MB (60.65%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -1,28 +1,9 @@
 class TreeAncestor {
 public:
-    static const int N=1e5+1,M=20;
+    static const int N=5e4+1,M=20;
     int Par[N][M];
-    vector<int> gr[N];
-    
-    void dfs(int cur, int p){
-        Par[cur][0]=p;
-        for(int j=1;j<M;j++){
-            Par[cur][j]=(Par[cur][j-1]>=0)?Par[Par[cur][j-1]][j-1]:-1;   
-        }
-        for(auto& x:gr[cur]){
-            if(x!=p)dfs(x,cur);
-        }
-    }
     
     TreeAncestor(int n, vector<int>& parent) {
-        // memset(Par,-1,sizeof(Par));
-        // for(int i=0;i<7;i++){
-        //     if(parent[i]>=0){
-        //         gr[parent[i]].push_back(i);
-        //         gr[i].push_back(parent[i]);
-        //     }
-        // }
-        // dfs(0,-1);
         for(int i=0;i<n;i++)Par[i][0]=parent[i];
         for(int j=1;j<M;j++){
             for(int i=0;i<n;i++){
