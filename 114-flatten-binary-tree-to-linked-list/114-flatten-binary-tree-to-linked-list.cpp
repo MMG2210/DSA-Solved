0ca6ab015c0b377fd9bcdc8e38ca89a11e7b805d
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-07-27 07:22:14 +0530

    Time: 13 ms (33.77%), Space: 12.6 MB (96.54%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -9,24 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void flatten(TreeNode* root) {
+//         if(!root)return;
+//         TreeNode* cur = root;
+//         while(cur!=NULL)
+//         {
+//             if(cur->left!=NULL)
+//             {
+//                 TreeNode *temp = cur->left;
+//                 while(temp->right!=NULL)
+//                     temp=temp->right;
+//                 temp->right = cur->right;
+//                 cur->right = cur->left;
+//                 cur->left = NULL;
+//                 cur=cur->right;
+//             }
+//             else cur=cur->right;
+//         }
+//     }
+// };O(N),O(1)
 class Solution {
 public:
+    vector<string> pre;
+    
+    void preOrder(TreeNode* root){
+        if(!root){
+            pre.push_back("$");
+            return;
+        }
+        preOrder(root->left);
+        pre.push_back(to_string(root->val));
+        preOrder(root->right);
+    }
+    
     void flatten(TreeNode* root) {
         if(!root)return;
-        TreeNode* cur = root;
-        while(cur!=NULL)
-        {
-            if(cur->left!=NULL)
-            {
-                TreeNode *temp = cur->left;
-                while(temp->right!=NULL)
-                    temp=temp->right;
-                temp->right = cur->right;
-                cur->right = cur->left;
-                cur->left = NULL;
-                cur=cur->right;
-            }
-            else cur=cur->right;
+        TreeNode* temp=root->right;
+        root->right=root->left;
+        root->left=nullptr;
+        TreeNode* node=root;
+        while(node->right!=NULL){
+            node=node->right;
         }
+        node->right=temp;
+        flatten(root->right);
     }
 };
