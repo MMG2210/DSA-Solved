80d371029c283b1b841576968dbd99474f9cfdb7
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-07-28 07:08:52 +0530

    Time: 13 ms (61.26%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(t.begin(),t.end());
-        sort(s.begin(),s.end());
-        return s==t;
+        // sort(t.begin(),t.end());
+        // sort(s.begin(),s.end());
+        // return s==t;
+        unordered_map<char,int> freq;
+        for(char&c:t)freq[c]++;
+        for(char& c:s){
+            freq[c]--;
+            if(freq[c]==0)freq.erase(c);
+        }
+        return freq.empty();
     }
 };
