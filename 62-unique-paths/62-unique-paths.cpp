4cf8c6224ed12d54bf4c8d572aafe0658091314a
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-08-01 12:27:57 +0530

    Time: 6 ms (12.58%), Space: 5.9 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[101][101];
     int helper(int i, int j,int m, int n, vector<vector<int>> &dp)
     {
         if(i>=m || j>=n)return 0;
@@ -8,10 +9,19 @@ public:
         if(dp[i][j]!=-1)return dp[i][j];
         else return dp[i][j] = helper(i+1,j,m,n,dp)+helper(i,j+1,m,n,dp);
     }
+    int helper(int i, int j, int m, int n)
+    {
+        if(i>=m || j>=n)return 0;
+        if(i==m-1 && j==n-1)return dp[i][j]=1;
+        
+        if(dp[i][j]!=-1)return dp[i][j];
+        else return dp[i][j] = helper(i+1,j,m,n)+helper(i,j+1,m,n);
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int> (n+1,-1));
-        //memset(dp,-1,sizeof(dp));
-        return helper(0,0,m,n,dp);
+        //vector<vector<int>> dp(m+1, vector<int> (n+1,-1));
+        memset(dp,-1,sizeof(dp));
+        //return helper(0,0,m,n,dp);
+        return helper(0,0,m,n);
         // int N = m+n-2;
         // int r = m-1;
         // double res = 1;
