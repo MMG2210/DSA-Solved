f59574b13d5f4adcb5bc45858d3f051a8a46fa62
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-08-01 12:25:59 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -6,17 +6,17 @@ public:
         if(i==m-1 && j==n-1)return dp[i][j]=1;
         
         if(dp[i][j]!=-1)return dp[i][j];
-        else return helper(i+1,j,m,n,dp)+helper(i,j+1,m,n,dp);
+        else return dp[i][j] = helper(i+1,j,m,n,dp)+helper(i,j+1,m,n,dp);
     }
     int uniquePaths(int m, int n) {
-        /*vector<vector<int>> dp(m+1, vector<int> (n+1,-1));
+        vector<vector<int>> dp(m+1, vector<int> (n+1,-1));
         //memset(dp,-1,sizeof(dp));
-        return helper(0,0,m,n,dp);*/
-        int N = m+n-2;
-        int r = m-1;
-        double res = 1;
-        for(int i = 1 ; i<=r ; i++)
-            res=res*(N-r+i)/i;
-        return (int)res;
+        return helper(0,0,m,n,dp);
+        // int N = m+n-2;
+        // int r = m-1;
+        // double res = 1;
+        // for(int i = 1 ; i<=r ; i++)
+        //     res=res*(N-r+i)/i;
+        // return (int)res;
     }
 };
