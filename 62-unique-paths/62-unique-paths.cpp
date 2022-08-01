62d551748b7dae7c78c81f2e5430064c8fe38601
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-08-01 12:38:44 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (63.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -31,13 +31,12 @@ public:
 //     }
        int uniquePaths(int m, int n){
            //vector<vector<int>> dp(m,vector<int>(n,1));
-           vector<int> pre(n,1),cur(n,1);
+           vector<int> cur(n,1);
            for(int i=1;i<m;i++){
                for(int j=1;j<n;j++){
-                   cur[j]=cur[j-1]+pre[j];
+                   cur[j]+=cur[j-1];
                }
-               swap(pre,cur);
            }
-           return pre[n-1];
+           return cur[n-1];
        }
 };
