156f347eaa716f6b3fb3dfa7b3416dd37f7c50c8
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-08-03 07:49:45 +0530

    Time: 95 ms (95.09%), Space: 38.8 MB (48.99%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,39 +1,14 @@
 class MyCalendar {
 public:
-    set<pair<int,int>> booked;
+    map<int,int> events;
     MyCalendar() {
         
     }
     
-    bool found(int st, int end, int k){
-        while(st<=end){
-            int mid=st + (end-st)/2;
-            if(mid==k)return true;
-            else if(mid>k)end=mid-1;
-            else st=mid+1;
-        }
-        return false;
-    }
-    
-    bool book(int s1, int e1) {
-        // for(auto it:booked){
-        //     int lS=it.first;
-        //     int lE=it.second;
-        //     // if(lE+1==start){
-        //     //     booked.erase({lS,lE});
-        //     //     booked.insert({lS,end-1});
-        //     //     return true;
-        //     // }
-        //     if(lE<start)continue;
-        //     for(int i=start;i<end;i++){
-        //         if(found(lS, lE, i))return false;
-        //     }
-        // }
-        for(auto e:booked){
-            int s2=e.first, e2=e.second;
-            if(s2<e1 && s1<e2)return false;
-        }
-        booked.insert({s1,e1});
+    bool book(int start, int end) {
+        auto it = events.upper_bound(start);
+        if(it!=events.end() && it->second<end)return false;
+        events[end]=start;
         return true;
     }
 };
