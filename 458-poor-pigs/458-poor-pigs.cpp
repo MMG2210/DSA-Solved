05c186852c2234b8ae975d49bc1e58d2fddee583
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-08-06 20:54:09 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (22.55%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,6 +1,26 @@
+//class Solution {
+// public:
+//     int MOD=1e9+7;
+//     int mpow(int a, int b){
+//         int res=1;
+//         while(b){
+//             if(b&1)res*=a;
+//             a*=a;
+//             b>>=1;
+//         }
+//         return res;
+//     }
+    
+//     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+//         int pigs=0, rounds=minutesToTest/minutesToDie;
+//         while(mpow(rounds+1,pigs)<buckets)pigs++;
+//         return pigs;
+//     }
+// };
+
 class Solution {
 public:
-    int MOD=1e9+7;
+    int rounds;
     int mpow(int a, int b){
         int res=1;
         while(b){
@@ -10,10 +30,17 @@ public:
         }
         return res;
     }
-    
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int pigs=0, rounds=minutesToTest/minutesToDie;
-        while(mpow(rounds+1,pigs)<buckets)pigs++;
-        return pigs;
+        int low=0, high=buckets, rounds = (minutesToTest/minutesToDie)+1;
+        int res=INT_MAX;
+        while(low<=high){
+            int pigs=(low+high)>>1;
+            if(pow(rounds,pigs)>=buckets){
+                res=pigs;
+                high=pigs-1;
+            }
+            else low=pigs+1;
+        }
+        return res;
     }
 };
