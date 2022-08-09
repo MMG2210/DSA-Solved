49bb5f1426b22e487cb12a5972a3d41d6ff9aea5
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-08-09 14:21:54 +0530

    Time: 105 ms (60.61%), Space: 9.3 MB (67.38%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -10,7 +10,8 @@ public:
             dp[A[i]]=1;
             for(int j=0;j<i;j++){
                 if(A[i]%A[j]==0){
-                    dp[A[i]] = (dp[A[i]] + dp[A[j]]*dp[A[i]/A[j]])%MOD;
+                    int r = A[i]/A[j];
+                    if(dp.count(r))dp[A[i]] = (dp[A[i]] + dp[A[j]]*dp[r])%MOD;
                 }
             }
             res = (res+dp[A[i]])%MOD;
