eaae679672b0c288ae5bc73902307769a17ee7ec
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-08-30 18:34:34 +0530

    Time: 46 ms (73.91%), Space: 13.9 MB (74.32%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,13 +3,7 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(a)
-        i = 0
-        j=n-1
-        while i<j:
-            for k in range(0,j-i):
-                a[i][i+k],a[i+k][j]=a[i+k][j],a[i][i+k]
-                a[i][i+k],a[j][j-k]=a[j][j-k],a[i][i+k]
-                a[i][i+k],a[j-k][i]=a[j-k][i],a[i][i+k]
-            i+=1
-            j-=1
+        a.reverse()
+        for i in range(len(a)):
+            for j in range(i):
+                a[i][j],a[j][i]=a[j][i],a[i][j]
