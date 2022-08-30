021a9d2ea21891732ed8bff327e87a212e3966b2
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-08-30 18:37:04 +0530

    Time: 8 ms (9.58%), Space: 7.2 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& a) {
-        int n = a.size();
         reverse(a.begin(),a.end());
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+        for(int i=0;i<a.size();i++){
+            for(int j=0;j<i;j++){
                 swap(a[i][j],a[j][i]);
             }
         }
