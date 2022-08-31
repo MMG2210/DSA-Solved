23522bd6c1d6e205116e50f95bbea5e0de26bc20
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-08-31 20:57:05 +0530

    Time: 48 ms (88.54%), Space: 23.9 MB (26.04%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-    int m, n;
-	// denotes cells reachable starting from atlantic and pacific edged cells respectively
-    vector<vector<bool> > atlantic, pacific;
-	vector<vector<int> > ans;    
-    vector<vector<int> > pacificAtlantic(vector<vector<int>>& mat) {
-        if(!size(mat)) return ans;
-        m = size(mat), n = size(mat[0]);
-        atlantic = pacific = vector<vector<bool> >(m, vector<bool>(n, false));
-		// perform dfs from all edge cells (ocean-connected cells)
-        for(int i = 0; i < m; i++) dfs(mat, pacific, i, 0), dfs(mat, atlantic, i, n - 1);
-        for(int i = 0; i < n; i++) dfs(mat, pacific, 0, i), dfs(mat, atlantic, m - 1, i);             
-        return ans;
+    int m,n;
+    vector<vector<int>> res;
+    vector<vector<bool>> atl,pac;
+    
+    void dfs(vector<vector<int>>& h, vector<vector<bool>>& vis, int i, int j){
+        if(vis[i][j])return;
+        vis[i][j]=1;
+        if(atl[i][j] && pac[i][j]){
+            res.push_back({i,j});
+        }
+        if(i+1<m && h[i+1][j]>=h[i][j])dfs(h,vis,i+1,j);
+        if(i-1>=0 && h[i-1][j]>=h[i][j])dfs(h,vis,i-1,j);
+        if(j+1<n && h[i][j+1]>=h[i][j])dfs(h,vis,i,j+1);
+        if(j-1>=0 && h[i][j-1]>=h[i][j])dfs(h,vis,i,j-1);
     }
-    void dfs(vector<vector<int> >& mat, vector<vector<bool> >& visited, int i, int j){        
-        if(visited[i][j]) return;
-        visited[i][j] = true;
-		// if cell reachable from both the oceans, insert into final answer vector
-        if(atlantic[i][j] && pacific[i][j]) ans.push_back(vector<int>{i, j});    
-		// dfs from current cell only if height of next cell is greater
-/*⬇️*/  if(i + 1 <  m && mat[i + 1][j] >= mat[i][j]) dfs(mat, visited, i + 1, j); 
-/*⬆️*/  if(i - 1 >= 0 && mat[i - 1][j] >= mat[i][j]) dfs(mat, visited, i - 1, j);
-/*➡️*/  if(j + 1 <  n && mat[i][j + 1] >= mat[i][j]) dfs(mat, visited, i, j + 1); 
-/*⬅️*/  if(j - 1 >= 0 && mat[i][j - 1] >= mat[i][j]) dfs(mat, visited, i, j - 1);
+    
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& h) {
+        if(h.empty()){
+            return res;
+        }
+        m=h.size(),n=h[0].size();
+        atl = pac = vector<vector<bool>>(m,vector<bool>(n,0));
+        for(int i=0;i<m;i++){
+            dfs(h,pac,i,0);
+            dfs(h,atl,i,n-1);
+        }
+        for(int i=0;i<n;i++){
+            dfs(h,pac,0,i);
+            dfs(h,atl,m-1,i);
+        }
+        return res;
     }
 };
