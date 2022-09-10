ec43be6e028fff2c5d759c7e8d397eecfd021ea1
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-09-10 12:05:34 +0530

    Time: 8 ms (87.87%), Space: 12.6 MB (46.87%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -14,6 +14,18 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         kt=2*k, N=prices.size();
         dp.resize(kt+1,vector<int>(N+1,-1));
+        
+        vector<int> memo(kt+1,INT_MIN);
+        memo[0]=0;
+        
+        for(int j=0;j<N;j++){
+            for(int i=0;i+2<=kt;i+=2){
+                memo[i+1]=max(memo[i+1],memo[i]-prices[j]);
+                memo[i+2]=max(memo[i+2],memo[i+1]+prices[j]);
+            }
+        }
+        
+        return *max_element(memo.begin(),memo.end());
         return solve(0,0,prices);
     }
 };
