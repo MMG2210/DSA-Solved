b68986f67d9cdada4e5354a18ea5b71107fec9d7
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-09-10 12:06:19 +0530

    Time: 11 ms (79.68%), Space: 10.6 MB (98.77%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int N,kt;
-    
-    int solve(int ind, int cur, vector<int>& p){
-        if(cur>kt || ind==N)return 0;
-        if(dp[cur][ind]!=-1)return dp[cur][ind];
-        int act = p[ind]*(cur&1?1:-1)+solve(ind+1,cur+1,p);
-        int hold = solve(ind+1,cur,p);
-        return dp[cur][ind]=max(act,hold);
-    }
-    
     int maxProfit(int k, vector<int>& prices) {
-        kt=2*k, N=prices.size();
-        dp.resize(kt+1,vector<int>(N+1,-1));
-        
+        int kt=2*k,N=prices.size();
         vector<int> memo(kt+1,INT_MIN);
         memo[0]=0;
         
@@ -26,6 +13,5 @@ public:
         }
         
         return *max_element(memo.begin(),memo.end());
-        return solve(0,0,prices);
     }
 };
