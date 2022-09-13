919af358dd28f9dd635cb88dde659947166fcf6c
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-09-13 16:00:33 +0530

    Time: 189 ms (77.10%), Space: 49.9 MB (61.45%) - LeetHub

--- 464-can-i-win/464-can-i-win.cpp
+++ 464-can-i-win/464-can-i-win.cpp
@@ -1,23 +1,23 @@
 class Solution {
 private:
     int m,t;
-    vector<vector<int>> dp;
-    bool game(int mask,int player,int score){
-        if(dp[player][mask]!=-1) return dp[player][mask];
+    vector<int> dp;
+    bool game(int mask,int score){
+        if(dp[mask]!=-1) return dp[mask];
         for(int i=0;i<m;i++)
         {
             int cmask = 1<<i;
             if( (mask&cmask) == 0 )
-                    if(score+i+1 >= t or game((mask|cmask),(player^1),score+i+1)==false ) return dp[player][mask]=1;
+                    if(score+i+1 >= t or game((mask|cmask),score+i+1)==false ) return dp[mask]=1;
         }
-        return dp[player][mask]=0;
+        return dp[mask]=0;
     }
 public:
     bool canIWin(int maxChoosableInteger, int desiredTotal) {
         m=maxChoosableInteger;
         t=desiredTotal;
         if((m*(m+1))/2 < t) return false;
-        dp.resize(2,vector<int>(1<<m,-1));
-        return game(0,0,0);
+        dp.resize(1<<m,-1);
+        return game(0,0);
     }
 };
