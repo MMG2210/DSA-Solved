a25ece20e850d25c74af2ec9760745852321edbe
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-09-15 13:52:36 +0530

    Time: 869 ms (25.53%), Space: 146.4 MB (35.65%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    vector<int> findOriginalArray(vector<int>& arr) {
+    vector<int> findOriginalArray(vector<int>& A) {
+        if (A.size() % 2) return {};
+        unordered_map<int, int> c;
+        for (int a : A) c[a]++;
+        vector<int> keys;
+        for (auto it : c)
+            keys.push_back(it.first);
+        sort(keys.begin(), keys.end(), [](int i, int j) {return abs(i) < abs(j);});
         vector<int> res;
-        int N=arr.size();
-        unordered_map<int,int> m;
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<N;i++){
-            //cout<<arr[i]<<' ';
-            m[arr[i]]++;
+        for (int x : keys) {
+            if (c[x] > c[2 * x]) return {};
+            for (int i = 0; i < c[x]; ++i, c[2 * x]--)
+                res.push_back(x);
         }
-        for(int i=0;i<N;i++){
-            if(m[arr[i]] && m[arr[i]<<1]){
-                if(arr[i]==arr[i]*2){
-                    if(m[arr[i]]<=1)continue;
-                }
-                m[arr[i]]--;
-                res.push_back(arr[i]);
-                m[arr[i]<<1]--;
-                if(m[arr[i]]==0)m.erase(arr[i]);
-                if(m[arr[i]<<1]==0)m.erase(arr[i]<<1);
-            }
-        }
-        if(res.size()<<1==N)return res;
-        else return {};
+        return res;
     }
 };
