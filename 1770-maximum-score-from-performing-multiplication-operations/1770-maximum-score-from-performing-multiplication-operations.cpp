ded1286c9010c90bffdf32c722faacae5dc0128a
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-09-16 15:04:27 +0530

    Time: 394 ms (94.93%), Space: 54.7 MB (98.40%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,19 +1,19 @@
-class Solution {
+class Solution{
 public:
-    vector<vector<int>> dp;
-    int dfs(vector<int>& nums, vector<int>& mults, int l, int i) {
-        if (i >= mults.size())
-            return 0;
-        if (dp[l][i]==INT_MIN) {
-            int r = nums.size() - 1 - (i - l);
-            dp[l][i] = max(nums[l] * mults[i] + dfs(nums, mults, l + 1, i + 1), 
-                nums[r] * mults[i] + dfs(nums, mults, l, i + 1));
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int n = (int)nums.size();
+        int m = (int)multipliers.size();
+        int dp[m + 1][m + 1] ;
+        memset(dp,0,sizeof(dp));
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int left = i; left >= 0; left--) {
+                int mult = multipliers[i];
+                int right = n - 1 - (i - left);
+                dp[i][left] = max(mult*nums[left] + dp[i + 1][left + 1], mult*nums[right] + dp[i + 1][left]) ; 
+            }
         }
-        return dp[l][i];
-    }
-    int maximumScore(vector<int>& nums, vector<int>& mults) {
-        int m=mults.size();
-        dp.resize(m+1,vector<int>(m+1,INT_MIN));
-        return dfs(nums, mults, 0, 0);
+        
+        return dp[0][0];
     }
 };
