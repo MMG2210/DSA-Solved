9f1465bfa80a268f8b60742a8b62cee0b1f88301
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-09-21 10:40:45 +0530

    Time: 227 ms (31.55%), Space: 46.2 MB (38.69%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -8,9 +8,9 @@ public:
         }
         for(auto& q:queries){
             int i=q[1],val=q[0];
-            if(nums[i]%2==0 && val%2==0)rsum+=val;
-            else if(nums[i]%2 && val%2)rsum+=(nums[i]+val);
-            else if(nums[i]%2==0 && val%2)rsum-=nums[i];
+            if(!(nums[i]&1) && !(val&1))rsum+=val;
+            else if(nums[i]&1 && val&1)rsum+=(nums[i]+val);
+            else if(!(nums[i]&1) && val&1)rsum-=nums[i];
             nums[i]+=val;
             res.push_back(rsum);
         }
