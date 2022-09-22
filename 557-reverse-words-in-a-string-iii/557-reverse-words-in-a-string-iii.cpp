22bdb7b087a294fa6f8620fb81210eb13e8ad679
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-09-22 20:49:50 +0530

    Time: 42 ms (20.37%), Space: 9.7 MB (75.20%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.size();
-        for(int i=0,j=0;j<=n;j++){
-            if(j==n || s[j]==' '){
+        for(int i=0,j=0;j<=s.size();j++){
+            if(j==s.size() || s[j]==' '){
                 reverse(s.begin()+i,s.begin()+j);
                 i=j+1;
             }
