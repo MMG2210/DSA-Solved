428e3269d8e2fcc5fa7c1e16790c095dfc6b3bf2
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-09-23 22:40:49 +0530

    Time: 41 ms (86.41%), Space: 6 MB (47.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int concatenatedBinary(int n) {
         int MOD=1e9+7;
-        long long res=0;
+        long res=0;
         for(int i=1;i<=n;i++){
             int len  = 32 - __builtin_clz(i);
             res = ((res<<len)|i)%MOD;
