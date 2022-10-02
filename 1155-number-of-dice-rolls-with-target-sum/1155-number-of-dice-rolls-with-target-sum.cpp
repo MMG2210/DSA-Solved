7f6fca7a070c63d2e713b2959c14122b5adaa47b
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-10-02 16:52:22 +0530

    Time: 106 ms (19.10%), Space: 5.8 MB (99.91%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,22 +1,17 @@
-#define ll long long
 class Solution {
 public:
     const int MOD = 1e9+7;
-    int N,K;
-    vector<vector<int>> dp;
-    int solve(int ind, int target){
-        if(ind==0 && target==0)return 1;
-        if(ind==0 || target<0)return 0;
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        int res=0;
-        for(int i=1;i<=K;i++){
-            res=(res%MOD+solve(ind-1,target-i)%MOD)%MOD;
+    int numRollsToTarget(int n, int f, int target) {
+        int dp[n+1][target+1];
+        memset(dp,0,sizeof(dp));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                for(int k=1;k<=f;k++){
+                    if(k<=j)dp[i][j] = (dp[i][j]%MOD + (dp[i-1][j-k])%MOD)%MOD;
+                }
+            }
         }
-        return dp[ind][target]=res;
-    }
-    int numRollsToTarget(int n, int k, int target) {
-        K=k;
-        dp.assign(n+1,vector<int>(target+1,-1));
-        return solve(n,target);
+        return dp[n][target];
     }
 };
