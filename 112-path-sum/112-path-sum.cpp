8c52d4fcd6f56febe92cbd8375cd03d84109cd64
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-10-04 20:14:21 +0530

    Time: 20 ms (48.60%), Space: 21.4 MB (38.76%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -14,7 +14,7 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root==nullptr)return false;
         if(targetSum==root->val && root->left==nullptr && root->right==nullptr)return true;
-        
-        return hasPathSum(root->left,targetSum-root->val)||hasPathSum(root->right,targetSum-root->val);
+        targetSum-=root->val;
+        return hasPathSum(root->left,targetSum)||hasPathSum(root->right,targetSum);
     }
 };
