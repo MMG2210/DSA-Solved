db8dbaf0dc0a5f4660e633b85f4495a7b4fc495b
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-10-17 09:06:00 +0530

    Time: 3 ms (80.43%), Space: 6.4 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
-    bool checkIfPangram(string sentence) {
-        unordered_set<char> st;
-        for(char& c:sentence)st.insert(c);
-        return st.size()==26;
+    bool checkIfPangram(string& sentence) {
+        int st[26]={0};
+        for(char& c:sentence)st[c-'a']++;
+        for(int i=0;i<26;i++){
+            if(!st[i])return false;
+        }
+        return true;
     }
 };
