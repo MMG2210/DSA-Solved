79248b1dcb5029d68133673941dcfc93f93f2954
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-10-24 17:22:30 +0530

    Time: 6 ms (53.83%), Space: 55.3 MB (18.05%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         int n = nums.length;
         int[] cnt  = new int[n+1];
-        Arrays.fill(cnt,0);
+        //Arrays.fill(cnt,0);
         for(int i:nums)cnt[i]++;
         int[] res = new int[2];
         for(int i=1;i<=n;i++){
