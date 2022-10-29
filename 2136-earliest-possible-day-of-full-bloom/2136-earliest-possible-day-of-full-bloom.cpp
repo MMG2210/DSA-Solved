707f1132eb01c80e9a9370ad07845bf6e1a1b2a3
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-10-29 21:01:35 +0530

    Time: 402 ms (85.74%), Space: 168.6 MB (57.23%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -3,15 +3,19 @@
 #define S second
 class Solution {
 public:
+    static bool cmp(pii& a, pii& b){
+        return a.F>b.F;
+    }
     int earliestFullBloom(vector<int>& pt, vector<int>& gt) {
         vector<pii> arr;
         for(int i=0;i<pt.size();i++){
             arr.push_back({gt[i],pt[i]});
         }
-        int res=0;
-        sort(arr.begin(),arr.end());
-        for(auto x:arr){
-            res = max(res,x.F)+x.S;
+        int res=0,days=0;
+        sort(arr.begin(),arr.end(), cmp);
+        for(int i=0;i<arr.size();i++){
+            days+=arr[i].S;
+            res = max(res,days+arr[i].F);
         }
         return res;
     }
