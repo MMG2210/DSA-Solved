6c27d413de3fd7398165f6c7bf4bae961245125e
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-11-01 13:03:55 +0530

    Time: 73 ms (10.06%), Space: 13.2 MB (52.18%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int M, N;
+    int dfs(vector<vector<int>>& grid, int row, int col){
+        if(row==M)return col;
+        int ncol = col+grid[row][col];
+        if(ncol<0 || ncol>N-1 || grid[row][col]!=grid[row][ncol])return -1;
+        return dfs(grid, row+1, ncol);
+    }
+    vector<int> findBall(vector<vector<int>>& grid) {
+        M = grid.size(), N=grid[0].size();
+        vector<int> res(N);
+        for(int j=0;j<N;j++){
+            res[j] = dfs(grid,0,j);
+        }
+        return res;
+    }
+};
