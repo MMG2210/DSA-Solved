007bd25cbd8d7fff31d8d475216a677239231872
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-11-02 14:56:15 +0530

    Time: 4 ms (29.93%), Space: 6.8 MB (7.33%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -0,0 +1,40 @@
+#define F first
+#define S second
+class Solution {
+public:
+    unordered_set<string> val;
+    int bfs(string& start, string& end){
+        queue<pair<string,int>> q;//Queue of (string and its level)
+        q.push({start,0});
+        vector<char> nxt;
+        nxt.push_back('A');
+        nxt.push_back('C');
+        nxt.push_back('G');
+        nxt.push_back('T');
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                auto node = q.front();q.pop();
+                if(node.F == end)return node.S;
+                for(int i=0;i<node.F.length();i++){
+                    char c = node.F[i];
+                    for(char& a:nxt){
+                        if(c==a)continue;
+                        node.F[i] = a;
+                        if(val.find(node.F)!=val.end()){
+                            q.push({node.F,node.S+1});
+                        }
+                        node.F[i]=c;
+                    }
+                }
+            }
+        }
+        return 1e9;
+    }
+    int minMutation(string start, string end, vector<string>& bank) {
+        val = unordered_set<string>(bank.begin(),bank.end());
+        if(val.find(end)==val.end())return -1;
+        int res = bfs(start, end);
+        return res==1e9?-1:res;
+    }
+};
