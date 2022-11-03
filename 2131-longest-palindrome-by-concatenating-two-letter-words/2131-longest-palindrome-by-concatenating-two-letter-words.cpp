d8cb853e55d405975a6da51a87d3d63b89d0b42a
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2022-11-03 07:27:38 +0530

    Time: 1094 ms (22.85%), Space: 211.5 MB (5.22%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        int res=0;
+        unordered_multiset<string> mp;
+        for(auto& i:words){
+            string cur = i;
+            swap(cur[0],cur[1]);
+            if(mp.find(cur)!=mp.end()){
+                res+=4;
+                mp.erase(mp.find(cur));
+            }
+            else mp.insert(i);
+        }
+        for(auto& s:mp){
+            if(s[0]==s[1]){
+                res+=2;
+                break;
+            }
+        }
+        return res;
+    }
+};
